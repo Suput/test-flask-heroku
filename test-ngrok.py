3e906d378aae655b009c05ab0881c16b516526a7
Nick <40500378+Suput@users.noreply.github.com>
2022-03-10 18:58:45 +0300

    Add files via upload

---
+++ test-ngrok.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/")
+def index():
+    return "<h1 style='font-size: 1000%'>Hello Internet</h1>"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
